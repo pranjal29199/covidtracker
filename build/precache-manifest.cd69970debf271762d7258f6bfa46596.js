5bf0544ca220cd446f6adf0161b9b8aa67067360
pranjal29199 <51282420+pranjal29199@users.noreply.github.com>
2020-05-21 20:44:29 +0530

    Add files via upload

---
+++ build/precache-manifest.cd69970debf271762d7258f6bfa46596.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d42b99b83556269d4d7954f5d928904e",
+    "url": "/covidtracker/index.html"
+  },
+  {
+    "revision": "c049f3f93fc55112fd7f",
+    "url": "/covidtracker/static/css/main.9aef48c1.chunk.css"
+  },
+  {
+    "revision": "d2fec483015962fcc031",
+    "url": "/covidtracker/static/js/2.130bce32.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covidtracker/static/js/2.130bce32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c049f3f93fc55112fd7f",
+    "url": "/covidtracker/static/js/main.cc309d20.chunk.js"
+  },
+  {
+    "revision": "de413a1930c6a774ec79",
+    "url": "/covidtracker/static/js/runtime-main.09cab292.js"
+  },
+  {
+    "revision": "5951fd8844edfa22f18439105aebef7a",
+    "url": "/covidtracker/static/media/image.5951fd88.png"
+  }
+]);
